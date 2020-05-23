f817370a63196734ec17f3287872d18191d3c760
SamuilNikolov <samuil_nikolov@abv.bg>
2020-05-23 14:49:26 +0300

    Create Cooking_by_numbers.js

---
+++ Cooking_by_numbers.js
@@ -0,0 +1,26 @@
+function cooking(arr){
+var num = parseInt(arr[0]);
+for(let i = 1; i<arr.length; i++){
+    switch(arr[i]){
+        case 'chop':
+            num/=2;
+            break;
+            case 'dice':
+                num = Math.sqrt(num);
+                break;
+                case 'spice':
+                    num+=1;
+                    break;
+                    case 'bake':
+                        num*=3;
+                        break;
+                        case 'fillet':
+                            num = num - num*0.2;
+                            break;
+        
+    }
+    console.log(num);
+}
+
+
+}
